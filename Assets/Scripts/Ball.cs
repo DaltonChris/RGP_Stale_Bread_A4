d3e990493b6454b91795d3acbb63a008102a67cf
DuckPeak <dylan.coon@student.torrens.edu.au>
2024-10-28 13:12:19 +1100

    Adjusted ball logic
    * Adjusted ball physics fixing an issue with colliding with top of cubes.
    * removed "constant speed" of ball since not needed anymore.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,8 +7,6 @@ public class Ball : MonoBehaviour
 {
     public event Action OnBallReset;  // Event to notify when the ball resets
 
-    public float constantSpeed = 10f;  // Desired constant speed
-
     private Rigidbody2D rb;
 
     void Start()
@@ -16,21 +14,9 @@ public class Ball : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
-    {
-        // Normalize the velocity to maintain constant speed
-        rb.velocity = rb.velocity.normalized * constantSpeed;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        // Ensure velocity maintains constant speed after collision
-        rb.velocity = rb.velocity.normalized * constantSpeed;
-    }
-
     private void OnBecameInvisible()
     {
-        // Reset the ball when it leaves the screen
+        // Notify the shooter to reset when the ball leaves the screen
         OnBallReset?.Invoke();
         Destroy(gameObject);
     }
