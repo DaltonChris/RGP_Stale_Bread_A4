37ea9bdebc725938a951acaa6e74a95690da6e8f
DaltonChris <daltonchris2@gmail.com>
2024-11-08 10:32:51 +1030

    Widen wall lvl 6

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -48,7 +48,7 @@ public class Ball : MonoBehaviour
     float shakeActiveValue = 1.2f;
 
     GameObject resetUI;
-    float resetVector = 0.035f;
+    float resetVector = 0.045f;
     float lowVelTimer = 0f; // Timer for low velocity
     float lowVelDuration = 1.95f;
     bool hasWon;
